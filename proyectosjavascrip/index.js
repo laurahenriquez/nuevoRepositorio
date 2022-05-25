778f58a3ff98a2dfb4c7f4737feb35dbf3bcf01d
laurahenriquez <laurahenriquez@gmail.com>
2022-05-25 19:55:35 +0200

    agrege prettier

--- proyectosjavascrip/index.js
+++ proyectosjavascrip/index.js
@@ -1,42 +1,26 @@
-function suma ( x, y =0)
-{
-    let resultadoSuma = x + y ;
-    console.log(resultadoSuma)
+function suma(x, y = 0) {
+  let resultadoSuma = x + y;
+  console.log(resultadoSuma);
 }
 
-function resta ( x, y = 0)
-{
-
-let resultadoResta = x - y ;
-console.log(resultadoResta)
-
+function resta(x, y = 0) {
+  let resultadoResta = x - y;
+  console.log(resultadoResta);
 }
 
-function multiplicacion ( x, y = 0 ) 
-{
-    let resultadoMultiplicacion = x * y ;
-    console.log(resultadoMultiplicacion)
-
+function multiplicacion(x, y = 0) {
+  let resultadoMultiplicacion = x * y;
+  console.log(resultadoMultiplicacion);
 }
 
-
-function division ( x, y=0)
-{
-    let resultadoDivision = x/y ;
-    console.log(resultadoDivision)
-
+function division(x, y = 0) {
+  let resultadoDivision = x / y;
+  console.log(resultadoDivision);
 }
 
-function cuadrado (x) 
-
-{
-    let cuadradoNumero = Math.pow (x,2);
-    console.log(cuadradoNumero)
-
+function cuadrado(x) {
+  let cuadradoNumero = Math.pow(x, 2);
+  console.log(cuadradoNumero);
 }
 
-
-
-
-
-module.exports = { suma, resta, multiplicacion, division , cuadrado }
+module.exports = { suma, resta, multiplicacion, division, cuadrado };
